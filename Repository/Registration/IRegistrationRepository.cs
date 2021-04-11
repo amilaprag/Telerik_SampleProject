507b63188a58651a57a792bfadcac0bcea053024
perumbuli <amila@ozenlabs.com>
2021-04-11 21:29:10 +0530

    registration repositotry int

--- Repository/Registration/IRegistrationRepository.cs
+++ Repository/Registration/IRegistrationRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telerik_SampleProject.Model;
 
 namespace Telerik_SampleProject.Repository.Registration
 {
-    interface IRegistrationRepository
+    public interface IRegistrationRepository
     {
+        public Task<Boolean> Registration(RegistrationModel Model);
     }
 }
--- Repository/Registration/RegistrationRepository.cs
+++ Repository/Registration/RegistrationRepository.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telerik_SampleProject.Model;
 
 namespace Telerik_SampleProject.Repository.Registration
 {
-    public class RegistrationRepository
+    public class RegistrationRepository : IRegistrationRepository
     {
+        public Task<bool> Registration(RegistrationModel Model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
