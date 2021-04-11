b90cfae94d2d3f8623824dc92240f9ac0a38b032
perumbuli <amila@ozenlabs.com>
2021-04-11 22:50:49 +0530

    Adding Database coniguration

---
+++ Database/DbConntext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Telerik_SampleProject.Database
+{
+    public class DatabaseConntext : DbContext
+    {
+        public DatabaseConntext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder Builder)
+        {
+            base.OnModelCreating(Builder);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telerik_SampleProject.Database;
 
 namespace Telerik_SampleProject
 {
@@ -26,6 +28,8 @@ namespace Telerik_SampleProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseConntext>(options => options.UseSqlServer(Configuration.GetConnectionString("")));
+
             // Add framework services.
             services
                 .AddControllersWithViews()
