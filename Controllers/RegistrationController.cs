2011c1e04cff7570564c0af610bd6b6f12282b77
perumbuli <amila@ozenlabs.com>
2021-04-11 12:36:22 +0530

    Add project files.

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telerik_SampleProject.Model;
+
+namespace Telerik_SampleProject.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: RegistrationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegistrationModel Model)
+        {
+            return View();
+        }
+
+        // GET: RegistrationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegistrationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Model/RegistrationModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik_SampleProject.Model
+{
+    public class RegistrationModel
+    {
+        [Key]
+        [Column("Reg_Id")]
+        public int RegistrationModelID { get; set; }
+
+        #region name
+        //TextBox
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, MinimumLength = 3,
+        ErrorMessage = "First Name should be minimum 3 characters and a maximum of 50 characters")]
+        [DataType(DataType.Text)]
+        [Column("Reg_Name")]
+        public string Name { get; set; }
+        #endregion
+
+        #region
+        //NumericTextBox
+        [Column("Reg_Age")]
+        [Required(ErrorMessage = "{0} is required")]
+        public int Age { get; set; }
+
+        #endregion
+
+        #region FirstName
+        //MaskedTextBox
+        [Column("Reg_FirstName")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string FirstName { get; set; }
+        #endregion
+
+        #region Date
+        //Calender
+        [Required(ErrorMessage = "Please select date")]
+        [DataType(DataType.Text)]
+        [Column("Reg_Date")]
+        public DateTime RegDate { get; set; }
+        #endregion
+
+
+    }
+}
