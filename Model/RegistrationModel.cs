4fad9020df6b72cea162fbf86d7c1caf55c18d03
perumbuli <amila@ozenlabs.com>
2021-04-11 21:25:52 +0530

    Form and model changes

--- Model/RegistrationModel.cs
+++ Model/RegistrationModel.cs
@@ -13,39 +13,56 @@ namespace Telerik_SampleProject.Model
         [Column("Reg_Id")]
         public int RegistrationModelID { get; set; }
 
-        #region name
-        //TextBox
-        [Required(ErrorMessage = "{0} is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "First Name should be minimum 3 characters and a maximum of 50 characters")]
+        #region Name
+        [StringLength(50, MinimumLength = 3,ErrorMessage = "First Name should be minimum 3 characters and a maximum of 50 characters")]
         [DataType(DataType.Text)]
         [Column("Reg_Name")]
         public string Name { get; set; }
         #endregion
 
-        #region
-        //NumericTextBox
+        #region Age
         [Column("Reg_Age")]
-        [Required(ErrorMessage = "{0} is required")]
+        [Range(0, 100)]
+        [Required(ErrorMessage = "Age is required")]
         public int Age { get; set; }
-
         #endregion
 
         #region FirstName
-        //MaskedTextBox
         [Column("Reg_FirstName")]
         [Required(ErrorMessage = "{0} is required")]
         public string FirstName { get; set; }
         #endregion
 
         #region Date
-        //Calender
         [Required(ErrorMessage = "Please select date")]
         [DataType(DataType.Text)]
         [Column("Reg_Date")]
         public DateTime RegDate { get; set; }
         #endregion
 
+        #region CivilStatus
+        [Required(ErrorMessage = "Please select date")]
+        [Column("Reg_CivilStatus")]
+        public bool CivilStatus { get; set; }
+        #endregion
+
+        #region Langudage
+        [DataType(DataType.Text)]
+        [Column("Reg_Language")]
+        public string Langudage { get; set; }
+        #endregion
+
+        #region Subscription
+        [Required(ErrorMessage = "Please select date")]
+        [Column("Reg_Subscription")]
+        public bool Subscription { get; set; }
+        #endregion
+
+        #region City
+        // [Required(ErrorMessage = "Please select a city")]
+        [Column("Reg_City")]
+        public String City { get; set; }
+        #endregion
 
     }
 }
